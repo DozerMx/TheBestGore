a47be5ee80235a5cb515d2f494f6d24326b43c7f
𝑫𝚯𝚭𝚭𝚬𝑅來𝚳𝚾 <101079820+DozerMx@users.noreply.github.com>
2024-10-28 01:59:05 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,28 @@
 // firebaseConfig.js
 
-// Importar las funciones necesarias de Firebase
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database"; // Para Realtime Database
-import { getAuth } from "firebase/auth"; // Para autenticación
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
-// Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD9Do3nEXf2QQWr4dKzrk2oyr_UB3ByTlE",
-  authDomain: "thebestgore-4bd6d.firebaseapp.com",
-  projectId: "thebestgore-4bd6d",
-  storageBucket: "thebestgore-4bd6d.appspot.com",
-  messagingSenderId: "615685617268",
-  appId: "1:615685617268:web:63c280e698eeaaf11b0321",
-  measurementId: "G-SXYJ04BTPM"
+    apiKey: "AIzaSyD9Do3nEXf2QQWr4dKzrk2oyr_UB3ByTlE",
+    authDomain: "thebestgore-4bd6d.firebaseapp.com",
+    projectId: "thebestgore-4bd6d",
+    storageBucket: "thebestgore-4bd6d.appspot.com",
+    messagingSenderId: "615685617268",
+    appId: "1:615685617268:web:63c280e698eeaaf11b0321",
+    measurementId: "G-SXYJ04BTPM",
+    databaseURL: "https://thebestgore-4bd6d-default-rtdb.firebaseio.com"
 };
 
-// Inicializar Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar la base de datos y la autenticación
-export const database = getDatabase(app);
-export const auth = getAuth(app);
+// Initialize services
+const auth = getAuth(app);
+const database = getDatabase(app);
+const storage = getStorage(app);
+
+// Export services
+export { auth, database, storage };
